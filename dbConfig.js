c8ffe3ef5ed90254e93e726bd6320b882160b30a
Ahmed Mujtaba <ahmedmujtaba0159@gmail.com>
2024-10-04 14:00:37 +0500

    Initial setup, added packages --Ahmed

---
+++ dbConfig.js
@@ -0,0 +1,14 @@
+const {Pool} = require('pg')
+
+const pool = new Pool({
+  user:"root_application",
+  host:"applications-10324.7tc.aws-eu-central-1.cockroachlabs.cloud",
+  password:"d67gCsGV8SuuKakhBuq_fw",
+  database:"Mflix_db",
+  port:"26257",
+  ssl:{
+    rejectUnauthorized:false
+  },
+})
+
+module.exports = pool
